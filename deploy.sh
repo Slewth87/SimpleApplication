2e8384cc3d64b1ebf7de499b1a1ea3b6bb3692ab
Richard Duffy <rduffy@slack-corp.com>
2022-03-16 13:56:00 +0000

    dummy app

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@ sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer.
 sudo npm install -g pm2
 # Stop any instance of our application currently running
-pm2 stop simpleapp
+pm2 stop example_app
 # Change Directory into folder where application is downloaded
-cd simpleapp
+cd rduffylab3
 # Install dependencies
 npm install
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 # Start with pm2
-pm2 start ./bin/www --name simpleapp
+pm2 start ./bin/www --name example_app
