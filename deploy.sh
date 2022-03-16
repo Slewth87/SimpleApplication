f82dfcaa87992d6fb439d7ab56c7e63a3761bdab
Richard Duffy <rduffy@slack-corp.com>
2022-03-16 14:07:09 +0000

    correct deply.sh

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@ sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer.
 sudo npm install -g pm2
 # Stop any instance of our application currently running
-pm2 stop example_app
+pm2 stop simpleapp
 # Change Directory into folder where application is downloaded
 cd rduffylab3
 # Install dependencies
 npm install
-echo $PRIVATE_KEY > privatekey.pem
-echo $SERVER > server.crt
+# echo $PRIVATE_KEY > privatekey.pem
+# echo $SERVER > server.crt
 # Start with pm2
 pwd
-pm2 start ./bin/www --name example_app
+pm2 start ./bin/www --name simpleapp
